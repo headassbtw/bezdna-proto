aa1934f8c8a376fbc6c774199a656b6b7dd33d96
headassbtw <cooldod909@gmail.com>
2021-12-07 10:16:20 -0800

    my laptop do be on fire tho

--- RPAK2L/Dialogs/HelpDialog/HelpContext.cs
+++ RPAK2L/Dialogs/HelpDialog/HelpContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using RPAK2L.ViewModels;
 using ReactiveUI;
 
@@ -6,5 +7,6 @@ namespace RPAK2L.Dialogs.HelpDialog
     public class HelpContext : ReactiveObject
     {
         public string Header => "Help";
+        public string VersionText => $"v{Assembly.GetExecutingAssembly().GetName().Version}";
     }
 }
---
+++ RPAK2L/Resources/HelpMenu/PatchNotes.md
@@ -0,0 +1,9 @@
+# Patch Notes #
+
+## 0.1.1
+* Fix to the settings menu
+* Added patch notes
+* Minor tweaks
+
+## 0.1.0
+* Initial stable release
